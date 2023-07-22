beebbc0d844b3b62db3ef52ef1991c85274a402e
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-22 07:43:40 +0200

    Today's Morning Commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "App";
 
 // Talent Verify React Context Provider
