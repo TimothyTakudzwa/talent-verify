79059d2d7a81e79395809f9e7ca090efe949c125
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-24 01:02:18 +0200

    Upload from csv implementation

--- src/layouts/departments/index.js
+++ src/layouts/departments/index.js
@@ -15,6 +15,7 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Stack from "@mui/material/Stack";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import CloseIcon from "@mui/icons-material/Close";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
@@ -27,6 +28,10 @@ import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 
 
 import { getDepartments, createDepartment } from "services/company";
@@ -41,10 +46,15 @@ function DepartmentsTable() {
   ];
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const handleClose = () => setOpen(false);
+  
   const [departmentName, setDepartmentName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [csvModalOpen, setCsvModalOpen] = useState(false);
+  const handleClose = () => {
+    setCsvModalOpen(false);
+  };
+  const [csvModal2Open, setCsvModal2Open] = useState(false);
   const [csvFile, setCsvFile] = useState(null);
 
 
@@ -117,10 +127,57 @@ function DepartmentsTable() {
     mr: 2,
     color: '#344767',
   };
+  const fileReader = new FileReader();
+  const [array, setArray] = useState([]);
+  const [csvDepartments, setCsvDepartments] = useState([]);
+  const csvFileToArray = string => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map(i => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
+    console.log(array);
+  };
+
+
 
   const handleCsvFileChange = (event) => {
     const file = event.target.files[0];
     setCsvFile(file);
+    // console.log("CSV file selected:", file);
+    fileReader.onload = function (event) {
+      const text = event.target.result;
+      csvFileToArray(text);
+    };
+
+    fileReader.readAsText(file);
+    console.log(array);
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+      const firstKey = Object.keys(array[i])[0];
+      const secondKey = Object.keys(array[i])[1];
+      const firstValue = array[i][firstKey];
+      const secondValue = array[i][secondKey];
+
+
+      newArray.push({ id: firstValue, name: secondValue });
+    }
+    // console.log(newArray);
+    setCsvDepartments(newArray);
+    console.log(csvDepartments);
+    setCsvModal2Open(true);
+
+
+    // print the file object on the console
+
   };
 
   const handleCsvModalClose = () => {
@@ -147,30 +204,108 @@ function DepartmentsTable() {
             Upload from Text
           </Button>
         </Stack>
-        <Dialog
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
           open={csvModalOpen}
-          onClose={handleCsvModalClose}
-          aria-labelledby="csv-upload-dialog-title"
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
         >
-          <DialogTitle id="csv-upload-dialog-title">Select CSV File</DialogTitle>
-          <DialogContent>
-            {/* Add your file input and other content for CSV file selection here */}
-            <input
-              type="file"
-              accept=".csv"
-              style={{ display: "none" }}
-              onChange={handleCsvFileChange}
+          <Fade in={csvModalOpen}>
+            <Box sx={style}>
+              <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", mb: 1 }}>
+                <Typography id="transition-modal-title" variant="h6" component="h2">
+                  Upload Departments from CSV
+                </Typography>
+                <Button variant="outlined" onClick={handleClose} sx={buttonStyle}>
+                  <CloseIcon />
+                </Button>
+              </Box>
+              <form>
+
+                <TextField
+                  id="department-name"
+                  type="file"
+
+                  variant="outlined"
+                  fullWidth
+                  value={departmentName}
+                  onChange={handleCsvFileChange}
+                  inputProps={{ accept: ".csv, text/csv" }}
+                  sx={{
+                    mt: 3,
+                    "& label": {
+                      fontSize: "14px", // Adjust the font size as needed
+                    },
+                  }}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleCreateDepartment}
+                  sx={{ mt: 3, width: '100%' }}
+                >
+                  {isLoading ? <CircularProgress size={24} /> : "Create Department"}
+                </Button>
+
+              </form>
+            </Box>
+          </Fade>
+        </Modal>
+        <Modal
+          open={csvModal2Open}
+          onClose={() => setCsvModal2Open(false)}
+          aria-labelledby="csv-upload-modal-title"
+        >
+          <Box sx={csvModalStyle}>
+            <Typography id="csv-upload-modal-title" variant="h6" component="h2">
+              Departments from CSV File
+            </Typography>
+            <DataGrid
+              // padding={10}
+
+              rows={onlineRows}
+              columns={onlineColumns}
+              // slots={{ toolbar: GridToolbar }}
+
+              onFilterModelChange={(model) => {
+
+                console.log('Filtering model changed:', model);
+              }}
+              initialState={{
+                pagination: {
+                  paginationModel: { page: 0, pageSize: 5 },
+                },
+              }}
+              pageSizeOptions={[5, 10, 20, 50, 100]}
+            // checkboxSelection
             />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCsvModalClose} color="primary">
-              Cancel
+            {/* <DataGrid
+            columns={onlineColumns}
+            rows={csvDepartments}           
+
+          <List>
+            {csvDepartments.map((dept, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={`ID: ${dept[Object.keys(dept)[0]]}, Name: ${dept[Object.keys(dept)[1]]}`} />
+              </ListItem>
+            ))}
+          </List> */}
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setCsvModal2Open(false)}
+            >
+              Close
             </Button>
-            <Button onClick={handleUploadCsv} color="primary">
-              Upload
-            </Button>
-          </DialogActions>
-        </Dialog>
+          </Box>
+        </Modal>
+
 
         <Modal
           aria-labelledby="transition-modal-title"
@@ -215,6 +350,7 @@ function DepartmentsTable() {
                 >
                   {isLoading ? <CircularProgress size={24} /> : "Create Department"}
                 </Button>
+
               </form>
             </Box>
           </Fade>
@@ -267,6 +403,16 @@ const style = {
   bgcolor: 'background.paper',
   border: '2px solid #000',
   // boxShadow: 24,
+  p: 4,
+};
+
+const csvModalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   p: 4,
 };
 // Departments component displays the Dashboard layout with the DepartmentsTable
