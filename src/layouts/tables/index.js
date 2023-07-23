5dfdd7d3854c8cc71a17de859a7f00f2bf5f31db
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-23 20:43:41 +0200

    Refactor Employees Page

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -1,185 +1,95 @@
-
-
-// @mui material components
+import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
-
-// Talent Verify React components
-import SoftBox from "components/SoftBox";
-import SoftTypography from "components/SoftTypography";
-
-// Talent Verify React examples
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
-import Table from "examples/Tables/Table";
-import { DataGrid, GridToolbar , GridValueGetterParams } from '@mui/x-data-grid';
-import { useEffect, useState } from "react";
-
-// Data
-import authorsTableData from "layouts/tables/data/authorsTableData";
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
 import projectsTableData from "layouts/tables/data/projectsTableData";
 import { getEmployees } from "services/company";
 
-function EmployeesTable({  }) {
-
-  
-  
-  const columns = [
-    { field: 'id', headerName: 'ID',flex: 1},
-    { field: 'firstName', headerName: 'First name',flex: 1 },
-    { field: 'lastName', headerName: 'Last name', flex: 1},
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      flex: 1
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      flex: 1,
-      sortable: false,
-     
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-
+// EmployeesTable component displays a DataGrid with online employee data
+function EmployeesTable() {
+  // Define the columns for the DataGrid
   const onlineColumns = [
-    { field: 'id', headerName: 'ID',flex: 1},
-    // name
-    { field: 'name', headerName: 'Full name',flex: 1 },
-    // department
-    // company
-    { field: 'company', headerName: 'Company', flex: 1},
-    { field: 'department', headerName: 'Department', flex: 1},
-    // role
-    { field: 'role', headerName: 'Role', flex: 1},
-    // date started
-    { field: 'date_started', headerName: 'Date started', flex: 1},
-    // date ended
-    { field: 'date_ended', headerName: 'Date ended', flex: 1},
-    // id number
-    { field: 'id_number', headerName: 'ID number', flex: 1},
+    { field: 'id', headerName: 'ID', flex: 1 },
+    { field: 'name', headerName: 'Full name', flex: 1 },
+    { field: 'company', headerName: 'Company', flex: 1 },
+    { field: 'department', headerName: 'Department', flex: 1 },
+    { field: 'role', headerName: 'Role', flex: 1 },
+    { field: 'date_started', headerName: 'Date started', flex: 1 },
+    { field: 'date_ended', headerName: 'Date ended', flex: 1 },
+    { field: 'id_number', headerName: 'ID number', flex: 1 },
   ];
 
-  const [onlineRows, setOnlineRows] = useState([])
+  // State to store online employee data
+  const [onlineRows, setOnlineRows] = useState([]);
 
   useEffect(() => {
+    // Fetch online employee data and update the state
     const fetchOnlineEmployees = async () => {
-      var response = await getEmployees();
-      var data = response;
-      console.log(data);
-      setOnlineRows(data)
-    }
-    fetchOnlineEmployees()
-  }, [])
+      try {
+        const response = await getEmployees();
+        setOnlineRows(response);
+      } catch (error) {
+        console.error("Error fetching employees:", error);
+      }
+    };
+    fetchOnlineEmployees();
+  }, []);
 
-
-
-  
   return (
     <Card>
-      <SoftBox  justifyContent="space-between"  p={3}>
+      <SoftBox justifyContent="space-between" p={3}>
         <SoftTypography variant="h6">Employees</SoftTypography>
       </SoftBox>
-      <div style={{  width: '100%' }}>
-      <SoftBox
-        sx={{
-          "& .MuiTableRow-root:not(:last-child)": {
-            "& td": {
-              borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                `${borderWidth[1]} solid ${borderColor}`,
-            },
-          },
-        }}
-      >
-        <DataGrid
-          rows={onlineRows}
-          columns={onlineColumns}
-          slots={{ toolbar: GridToolbar }}
-          filterModel={{
-            items: [{ columnField: 'firstName', operatorValue: 'contains', value: '' }],
-          }}
-          onFilterModelChange={(model) => {
-            // You can handle the filtering logic here if needed
-            console.log('Filtering model changed:', model);
-          }}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
+      <div style={{ width: '100%' }}>
+        <SoftBox
+          sx={{
+            "& .MuiTableRow-root:not(:last-child)": {
+              "& td": {
+                borderBottom: ({ borders: { borderWidth, borderColor } }) =>
+                  `${borderWidth[1]} solid ${borderColor}`,
+              },
             },
           }}
-          pageSizeOptions={[5, 10, 20, 50, 100]}
-          checkboxSelection
-        />
-      </SoftBox>
+        >
+          <DataGrid
+            rows={onlineRows}
+            columns={onlineColumns}
+            slots={{ toolbar: GridToolbar }}
+            filterModel={{
+              items: [{ columnField: 'firstName', operatorValue: 'contains', value: '' }],
+            }}
+            onFilterModelChange={(model) => {
+              // You can handle the filtering logic here if needed
+              console.log('Filtering model changed:', model);
+            }}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10, 20, 50, 100]}
+            checkboxSelection
+          />
+        </SoftBox>
       </div>
     </Card>
   );
 }
 
+// Tables component displays the Dashboard layout with the EmployeesTable
 function Tables() {
- 
-  const { columns: prCols, rows: prRows } = projectsTableData;
-  
-
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <SoftBox py={3}>
         <SoftBox mb={3}>
-          <Card>
-            <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
-              <SoftTypography variant="h6">Employees</SoftTypography>
-              
-              
-            </SoftBox>
-            <SoftBox
-              sx={{
-                "& .MuiTableRow-root:not(:last-child)": {
-                  "& td": {
-                    borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                      `${borderWidth[1]} solid ${borderColor}`,
-                  },
-                },
-              }}
-            >
-              <EmployeesTable />
-
-            </SoftBox>
-          </Card>
+          {/* Render the EmployeesTable component */}
+          <EmployeesTable />
         </SoftBox>
-        <Card>
-          <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
-            <SoftTypography variant="h6">Projects table</SoftTypography>
-          </SoftBox>
-          <SoftBox
-            sx={{
-              "& .MuiTableRow-root:not(:last-child)": {
-                "& td": {
-                  borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                    `${borderWidth[1]} solid ${borderColor}`,
-                },
-              },
-            }}
-          >
-            <Table columns={prCols} rows={prRows} />
-          </SoftBox>
-        </Card>
       </SoftBox>
       <Footer />
     </DashboardLayout>
