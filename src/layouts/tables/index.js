23959ebde77fc2868efdbe85a419cdbc9ac7a200
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-23 18:57:52 +0200

    Sunday Commit

--- src/layouts/tables/index.js
+++ src/layouts/tables/index.js
@@ -12,14 +12,88 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import Table from "examples/Tables/Table";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 // Data
 import authorsTableData from "layouts/tables/data/authorsTableData";
 import projectsTableData from "layouts/tables/data/projectsTableData";
 
+function EmployeesTable({  }) {
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+  
+  return (
+    <Card>
+      <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+        <SoftTypography variant="h6">Employees</SoftTypography>
+      </SoftBox>
+      <SoftBox
+        sx={{
+          "& .MuiTableRow-root:not(:last-child)": {
+            "& td": {
+              borderBottom: ({ borders: { borderWidth, borderColor } }) =>
+                `${borderWidth[1]} solid ${borderColor}`,
+            },
+          },
+        }}
+      >
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          filterModel={{
+            items: [{ columnField: 'firstName', operatorValue: 'contains', value: '' }],
+          }}
+          onFilterModelChange={(model) => {
+            // You can handle the filtering logic here if needed
+            console.log('Filtering model changed:', model);
+          }}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </SoftBox>
+    </Card>
+  );
+}
+
 function Tables() {
-  const { columns, rows } = authorsTableData;
+ 
   const { columns: prCols, rows: prRows } = projectsTableData;
+  
 
   return (
     <DashboardLayout>
@@ -28,7 +102,9 @@ function Tables() {
         <SoftBox mb={3}>
           <Card>
             <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
-              <SoftTypography variant="h6">Authors table</SoftTypography>
+              <SoftTypography variant="h6">Employees</SoftTypography>
+              
+              
             </SoftBox>
             <SoftBox
               sx={{
@@ -40,7 +116,8 @@ function Tables() {
                 },
               }}
             >
-              <Table columns={columns} rows={rows} />
+              <EmployeesTable />
+
             </SoftBox>
           </Card>
         </SoftBox>
