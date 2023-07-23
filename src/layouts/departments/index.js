32e8e2cadbad577e2c492aff2eac4db9b918b948
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-23 23:35:48 +0200

    Add Department

--- src/layouts/departments/index.js
+++ src/layouts/departments/index.js
@@ -13,52 +13,85 @@ import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+// CircularProgress
+import CircularProgress from '@mui/material/CircularProgress';
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 
-import { getDepartments } from "services/company";
-
+import { getDepartments, createDepartment } from "services/company";
+const MySwal = withReactContent(Swal)
 // DepartmentsTable component displays a DataGrid with online employee data
 function DepartmentsTable() {
   // Define the columns for the DataGrid
   const onlineColumns = [
-    { field: 'id', headerName: 'ID', flex: 1 },
-    { field: 'name', headerName: 'Full name', flex: 1 },
+    { field: 'id', headerName: 'ID', flex: 1, headerClassName: 'super-app-theme--header', },
+    { field: 'name', headerName: 'Department name', flex: 1, headerClassName: 'super-app-theme--header', },
 
   ];
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [departmentName, setDepartmentName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   // State to store online employee data
   const [onlineRows, setOnlineRows] = useState([]);
 
+  const fetchOnlineDepartments = async () => {
+    try {
+      const response = await getDepartments();
+      setOnlineRows(response);
+    } catch (error) {
+      console.error("Error fetching employees:", error);
+    }
+  };
   useEffect(() => {
     // Fetch online employee data and update the state
-    const fetchOnlineDepartments = async () => {
-      try {
-        const response = await getDepartments();
-        setOnlineRows(response);
-      } catch (error) {
-        console.error("Error fetching employees:", error);
-      }
-    };
+
     fetchOnlineDepartments();
   }, []);
 
-  const handleCreateDepartment = () => {
-    // Perform your create department action here with departmentName
-    console.log("Creating department with name:", departmentName);
-
-    // After the action is performed, close the modal
+  const handleCreateDepartment = async () => {
+    setIsLoading(true);
+    console.log(departmentName);
+    console.log("Form submitted:", localStorage.getItem("company"));
+    const data = {
+      name: departmentName,
+      company: 1
+    }
+    try {
+      const response = await createDepartment(data);
+      console.log("Create Department response:", response);
+      MySwal.fire(
+        'Success!',
+        'Department created successfully!',
+        'success'
+      )
+    }
+    catch (error) {
+      MySwal.fire(
+        'Error!',
+        'Department could not be created!',
+        'error'
+      );
+      console.error("Error creating department:", error);
+    }
+    fetchOnlineDepartments();
+    setIsLoading(false);
+    setDepartmentName("");
     handleClose();
+
+
   };
 
   return (
     <Card>
-      <SoftBox justifyContent="space-between" p={3}>
+      <SoftBox justifyContent="space-between" p={2}>
 
-        <Button onClick={handleOpen}>Create Department</Button>
+        <Button onClick={handleOpen} sx={buttonStyle}> <AddIcon/> Create Department</Button>
         <Modal
           aria-labelledby="transition-modal-title"
           aria-describedby="transition-modal-description"
@@ -80,27 +113,27 @@ function DepartmentsTable() {
 
               <form>
 
-              <TextField
-  id="department-name"
-  label="Department Name"
-  variant="outlined"
-  fullWidth
-  value={departmentName}
-  onChange={(e) => setDepartmentName(e.target.value)}
-  sx={{
-    mt: 3,
-    "& label": {
-      fontSize: "14px", // Adjust the font size as needed
-    },
-  }}
-/>
+                <TextField
+                  id="department-name"
+                  label="Department Name"
+                  variant="outlined"
+                  fullWidth
+                  value={departmentName}
+                  onChange={(e) => setDepartmentName(e.target.value)}
+                  sx={{
+                    mt: 3,
+                    "& label": {
+                      fontSize: "14px", // Adjust the font size as needed
+                    },
+                  }}
+                />
                 <Button
                   variant="contained"
                   color="primary"
                   onClick={handleCreateDepartment}
                   sx={{ mt: 3, width: '100%' }}
                 >
-                  Create Department
+                  {isLoading ? <CircularProgress size={24} /> : "Create Department"}
                 </Button>
               </form>
             </Box>
@@ -111,6 +144,8 @@ function DepartmentsTable() {
       <div style={{ width: '100%' }}>
         <SoftBox
           sx={{
+            padding: 2,
+            
             "& .MuiTableRow-root:not(:last-child)": {
               "& td": {
                 borderBottom: ({ borders: { borderWidth, borderColor } }) =>
@@ -119,30 +154,30 @@ function DepartmentsTable() {
             },
           }}
         >
-          <DataGrid
+          <DataGrid 
+          // padding={10}
+          
             rows={onlineRows}
             columns={onlineColumns}
             slots={{ toolbar: GridToolbar }}
-            filterModel={{
-              items: [{ columnField: 'firstName', operatorValue: 'contains', value: '' }],
-            }}
+
             onFilterModelChange={(model) => {
-              // You can handle the filtering logic here if needed
+
               console.log('Filtering model changed:', model);
             }}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-              },
-            }}
-            pageSizeOptions={[5, 10, 20, 50, 100]}
-            checkboxSelection
+           autoPageSize
           />
         </SoftBox>
       </div>
     </Card>
   );
 }
+
+const buttonStyle = {
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+};
 const style = {
   position: 'absolute',
   top: '50%',
@@ -161,7 +196,14 @@ function Departments() {
     <DashboardLayout>
       {/* <DashboardNavbar /> */}
       <SoftBox py={3}>
-        <SoftBox mb={3}>
+        <SoftBox mb={3} sx={{
+       
+        width: '100%',
+        '& .super-app-theme--header': {
+          backgroundColor: '#344767',
+          color: '#ffffff',
+        },
+      }}>
           {/* Render the DepartmentsTable component */}
           <DepartmentsTable />
         </SoftBox>
