f3de4de659547bfd2b9f446b9251a0a8fc6c9638
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-24 18:30:35 +0200

    Pagination fix

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -204,10 +204,10 @@ function EmployeesTable() {
             }}
             initialState={{
               pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
+                paginationModel: { page: 0, pageSize: 10 },
               },
             }}
-            pageSizeOptions={[5, 10, 20, 50, 100]}
+            // pageSizeOptions={[5, 10, 20, 50, 100]}
 
           />
           {viewEmployeeModal(viewEmployeeModalOpen, style, closeModals, buttonStyle, employeeData, setEmployeeData, departments, handleCreateEmployee, isLoading)}
